f31f46f435fa9f393a505e9f752cb529afc966f5
Neethu <neethu.somasekharan@sreyo.com>
2021-04-04 12:58:17 -0700

    first commit

---
+++ authentication/public/js/scripts.js
@@ -0,0 +1,43 @@
+
+$("form[name=signup_form").submit(function(e) {
+
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url: "/register",
+        type : "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp) {
+            $error.text('Success').removeClass("error--hidden");
+        },
+        error: function(resp){    
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+    e.preventDefault();
+});
+
+
+$("form[name=login_form").submit(function(e) {
+
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url: "/login",
+        type : "POST",
+        data: data,
+        dataType: "json",   
+        success: function(resp) {
+            $error.text('Success').removeClass("error--hidden");
+        },
+        error: function(resp){    
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+    e.preventDefault();
+});
